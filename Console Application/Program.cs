8e0bc6672b5e8524b1b1614f70ad1132f098b14e
sinokblya <avanpavlov0707@gmail.com>
2023-09-17 18:53:36 +0300

    Добавьте файлы проекта.

---
+++ Console Application/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "MyPipe", PipeDirection.InOut))
+        {
+            Console.WriteLine("Подключение к серверу...");
+            pipeClient.Connect();
+
+            Console.WriteLine("Сервер подключен.");
+
+            // Отправка данных серверу
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            pipeClient.Write(buffer, 0, buffer.Length);
+
+            pipeClient.WaitForPipeDrain();
+
+            // Чтение ответа от сервера
+            byte[] responseBuffer = new byte[256];
+            int bytesRead = pipeClient.Read(responseBuffer, 0, responseBuffer.Length);
+
+            string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+            Console.WriteLine("Получено от сервера: " + response);
+        }
+
+        Console.WriteLine("Клиент завершил работу.");
+    }
+}
